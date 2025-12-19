3cae302537b42579f8d7d2dbe641af6278bbd5f2
rakeshmgb <rakeshmgb@gmail.com>
2025-12-19 16:38:49 +0000

    Refactoring

--- pages/.well-known/openai-apps-challenge/route.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  res.status(200).setHeader("Content-Type", "text/plain");
-  res.send("AIoP_R6STqSRPySYC-pSOB-88psw-p5ewTFN9Fj_SUU");
-}
